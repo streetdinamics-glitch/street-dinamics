0e9d6ef2c30cc5b0a8ea57a30d907cb2c56ff850
STREET DINAMICS <streetdinamics@gmail.com>
2026-03-14 21:15:02 +0000

    File changes

--- src/components/cyber/RegistrationModal.jsx
+++ src/components/cyber/RegistrationModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useTranslation } from '../translations';
