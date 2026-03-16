7be143218a7f7749cc9d63a19ffab9541591891b
STREET DINAMICS <streetdinamics@gmail.com>
2026-03-16 15:03:10 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import WalletProvider from './components/web3/WalletProvider'
+import WalletProvider from './components/web3/WalletProvider.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
