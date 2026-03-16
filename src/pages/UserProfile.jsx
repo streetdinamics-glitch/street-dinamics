8a2015b52838fbf2969bfc8b8528a656cd606dc1
STREET DINAMICS <streetdinamics@gmail.com>
2026-03-16 06:19:32 +0000

    File changes

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -22,11 +22,10 @@ export default function UserProfile() {
   return (
     <div className="relative min-h-screen bg-cyber-void text-[var(--text-main)]">
       <CyberOverlays />
-      <Navbar lang={lang} onLangSwitch={setLang} />
+      <Navbar lang={lang} onLangSwitch={setLang} onScrollTo={() => {}} />
 
-      <div className="min-h-screen p-6">
+      <div className="pt-[80px] p-6">
         <div className="max-w-6xl mx-auto">
-          <div className="absolute top-0 left-0 right-0 fire-line" />
           <UserProfileDashboard lang={lang} />
         </div>
       </div>
